9fbfd0cf384a7bdd9cc7af51bff76927119d512b
hotaruZzZz <gfri5469@gmail.com>
2023-02-21 10:58:16 +0800

    加入專案檔案。

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("請輸入一串數字，並以逗號分隔: ");
+            s = Console.ReadLine();
+            var n = s.Split(',');
+            var reverse = n.OrderByDescending(x => x);
+            foreach ( var x in reverse) { Console.Write($"{x} "); }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("請輸入一串數字，並以逗號分隔: ");
+            s = Console.ReadLine();
+            var n = s.Split(',');
+            var odd = n.Where(x => int.Parse(x) % 2 != 0).OrderBy(x => x);
+            var even = n.Where(x => int.Parse(x) % 2 == 0).OrderBy(x => x);
+            Console.Write("奇數: ");
+            foreach (var x in odd) Console.Write($"{x} ");
+            Console.Write("\n偶數: ");
+            foreach (var x in even) Console.Write($"{x} ");
+
+            Console.ReadKey();
+        }
+    }
+}
